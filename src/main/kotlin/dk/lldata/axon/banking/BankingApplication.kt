9228ff921ed3472eacc73a586636bdba355eb2f4
SniperXDev <abuntrock3@gmail.com>
2017-11-19 22:09:01 +0100

    spring boot autoconfigure + jpa works

--- src/main/kotlin/dk/lldata/axon/banking/BankingApplication.kt
+++ src/main/kotlin/dk/lldata/axon/banking/BankingApplication.kt
@@ -1,51 +1,13 @@
 package dk.lldata.axon.banking
 
-import dk.lldata.axon.banking.account.Account
-import dk.lldata.axon.banking.coreapi.CreateAccountCommand
 import org.axonframework.commandhandling.CommandBus
-import org.axonframework.commandhandling.GenericCommandMessage.asCommandMessage
-import org.axonframework.commandhandling.callbacks.LoggingCallback
-import org.axonframework.commandhandling.model.GenericJpaRepository
-import org.axonframework.common.jpa.ContainerManagedEntityManagerProvider
-import org.axonframework.common.jpa.EntityManagerProvider
-import org.axonframework.common.transaction.TransactionManager
-import org.axonframework.eventhandling.EventBus
-import org.axonframework.eventhandling.saga.repository.jpa.JpaSagaStore
-import org.axonframework.eventsourcing.eventstore.jpa.DomainEventEntry
-import org.axonframework.eventsourcing.eventstore.jpa.JpaEventStorageEngine
-import org.axonframework.serialization.xml.XStreamSerializer
 import org.axonframework.spring.config.EnableAxon
-import org.axonframework.spring.messaging.unitofwork.SpringTransactionManager
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
-import org.springframework.transaction.PlatformTransactionManager
 
-@EnableAxon
 @SpringBootApplication
-//@EntityScan(basePackages = arrayOf("org.axonframework", "dk.lldata"))
-//@EntityScan(basePackages = arrayOf("dk.lldata"))
 class BankingApplication {
 
-  @Bean
-  fun eventStorageEngine(emp: EntityManagerProvider, tx : TransactionManager) =
-      JpaEventStorageEngine(emp, tx)
-
-//  @Bean
-//  fun sagaStore(emp : EntityManagerProvider) = JpaSagaStore(emp)
-
-  @Bean
-  fun axonTransactionManager(tx : PlatformTransactionManager) = SpringTransactionManager(tx)
-
-  @Bean
-  fun accountRepo(eventBus : EventBus) = GenericJpaRepository(entityManagerProvider(), Account::class.java, eventBus)
-
-  @Bean
-  fun entityManagerProvider() = ContainerManagedEntityManagerProvider()
-
-  @Bean
-  fun serializer() = XStreamSerializer()
 }
 
 fun main(args: Array<String>) {
--- src/main/kotlin/dk/lldata/axon/banking/rest/TestRestService.kt
+++ src/main/kotlin/dk/lldata/axon/banking/rest/TestRestService.kt
@@ -4,6 +4,7 @@ import dk.lldata.axon.banking.coreapi.CreateAccountCommand
 import dk.lldata.axon.banking.coreapi.ID
 import dk.lldata.axon.banking.coreapi.RequestMoneyTransferCommand
 import org.axonframework.commandhandling.gateway.CommandGateway
+import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping(path= arrayOf("/test"))
 class TestRestService(val commandGateway: CommandGateway) {
+  @Transactional
   @RequestMapping(path= arrayOf("/create/{accountId}"), method = arrayOf(RequestMethod.GET))
   fun create(@PathVariable accountId : String) {
     commandGateway.send<CreateAccountCommand>(CreateAccountCommand(accountId, 1000))
