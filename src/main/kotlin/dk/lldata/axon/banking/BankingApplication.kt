0b5e83c6c90c23b3784ea9c303b3018e611b1d25
SniperXDev <abuntrock3@gmail.com>
2017-11-19 16:33:12 +0100

    configure as spring boot application

--- src/main/kotlin/dk/lldata/axon/banking/BankingApplication.kt
+++ src/main/kotlin/dk/lldata/axon/banking/BankingApplication.kt
@@ -1,11 +1,20 @@
 package dk.lldata.axon.banking
 
+import org.axonframework.eventsourcing.eventstore.inmemory.InMemoryEventStorageEngine
+import org.axonframework.spring.config.EnableAxon
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
+@EnableAxon
 @SpringBootApplication
-class BankingApplication
+class BankingApplication {
+
+  @Bean
+  fun eventStorageEngine() = InMemoryEventStorageEngine()
+}
 
 fun main(args: Array<String>) {
-    runApplication<BankingApplication>(*args)
+  runApplication<BankingApplication>(*args)
 }
+
--- src/main/kotlin/dk/lldata/axon/banking/account/AccountAggregate.kt
+++ src/main/kotlin/dk/lldata/axon/banking/account/AccountAggregate.kt
@@ -8,7 +8,9 @@ import org.axonframework.commandhandling.CommandHandler
 import org.axonframework.commandhandling.model.AggregateIdentifier
 import org.axonframework.commandhandling.model.AggregateLifecycle
 import org.axonframework.eventsourcing.EventSourcingHandler
+import org.axonframework.spring.stereotype.Aggregate
 
+@Aggregate
 class Account {
   constructor() {}
 
